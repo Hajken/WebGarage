5fb183b3142ecbfc6623954e4e34444e572fad97
Aryo Pehlewan <aryopehlewan@hotmail.com>
2016-05-13 15:28:54 +0200

    Added route to Admin

--- WebGarage/App_Start/RouteConfig.cs
+++ WebGarage/App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace WebGarage
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                "Admin",                                           // Route name
+                "Admin",                            // URL with parameters
+                new { controller = "Home", action = "Admin" }  // Parameter defaults
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
